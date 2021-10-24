67abfca5d169e83fccd346d1d98f7c487c07ec01
docker-TYR <docker210106@tyr-tech.com>
2021-10-24 15:38:42 +0800

    [add] antd icon

--- web/src/App.vue
+++ web/src/App.vue
@@ -5,17 +5,22 @@ a-layout.h-screen
   a-layout-content
     router-view.h-full
   a-layout-footer.p-0
-    .notice.flex.justify-center.bg-red-400.items-center.text-white.py-1
+    .notice.flex.justify-between.bg-red-400.items-center.text-white.p-2
       span.text-white 注意事項：此站僅供使用者方便查看中籤號碼，非官方網站，中籤號碼暫時為手動更新，若有資料錯誤或未更新狀況，請以
-        a(target='_blank' href='https://vhpi.5000.gov.tw/')
+        a.text-blue-400.border-b-2.border-blue-400(target='_blank' href='https://vhpi.5000.gov.tw/')
           | 五倍加碼券官網
         | 為主。
+      GithubOutlined
     .create.flex.justify-center.bg-black.items-center.text-white.py-1
       strong 2021 — JaoJaoLin 傑姆士
 </template>
 <script>
+import { GithubOutlined } from "@ant-design/icons-vue"
 export default ({
   name: "App",
+  components: {
+    GithubOutlined
+  },
   setup() {}
 });
 </script>
